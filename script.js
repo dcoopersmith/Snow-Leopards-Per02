08252c79fb7d4f714dd2f29d4e11c0d3e7367e65
David Coopersmith <dcoopersmith@mvusd.net>
2025-10-23 13:35:01 -0700

    added a div to quiz.html and wrote 2 questions

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const Questions = [
+const Questions = [   
     {
         q: "Fill in the blank: Snow leopards live _________.",
-        
+        a: [{ text: "in the desert.", isCorrect: false },
+            { text: "in the jungle.", isCorrect: false },
+            { text: "in the mountains.", isCorrect: true },
+            { text: "in the city.", isCorrect: false },
+           ]        
     },
     {
-
+        q: "Fill in the blank: Snow leopards have _________.",
+        a: [{ text: "red, with whitish fur.", isCorrect: false },
+            { text: "whitish-gray fur.", isCorrect: true },
+            { text: "black stripes, with orange fur.", isCorrect: false },
+            { text: "white dots with black fur.", isCorrect: false },
+           ]  
     }
 
-
+    //  ToDo: finish questions....
 
 
 ];
+
+let currQuestion = 0;
+let score = 0;
+
+function loadQues()
+{
+    const question = document.getElementById("ques");
+}
 
 
